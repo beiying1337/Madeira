eae38e0567180c74b72561ada5f87500c8ecbe03
beiying1337 <beiying1337@icloud.com>
2026-09-11 22:27:30 +0800

    ci: generate D3D11 SDK layers header

--- scripts/ci-build-dependencies.sh
+++ scripts/ci-build-dependencies.sh
@@ -59,6 +59,7 @@ WINE_IDL_HEADERS=(
     include/d3d10shader.h
     include/d3d10effect.h
     include/d3d10_1.h
+    include/d3d11sdklayers.h
     include/d3d11.h
     include/d3d12sdklayers.h
     include/d3d12.h
